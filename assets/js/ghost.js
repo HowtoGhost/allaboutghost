fd22f9e90f0d83f9ce2795e9d7a818c43bde7824
David Balderston <dbalders@gmail.com>
2014-11-12 21:46:30 -0800

    Beginning Framework

---
+++ assets/js/ghost.js
@@ -0,0 +1,17 @@
+{
+ "name": "Ghost",
+ "children": [
+  {
+    "name": "bower.json",
+   "children": [
+    {"name": "Description of bower.json"}
+   ]
+  },
+  {
+   "name": "config.example.js",
+   "children": [
+    {"name": "Description of config.example.js"}
+   ]
+  }
+ ]
+}
