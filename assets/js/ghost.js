1bf4b02633cf82be7d1eb3484a31300091e1f765
David Balderston <dbalders@gmail.com>
2014-11-15 00:07:27 -0800

    Theme Files Added In

--- assets/js/ghost.js
+++ assets/js/ghost.js
@@ -20,6 +20,106 @@
      "children": [
       {"name": "README.md"}
      ]
+    },
+    {"name": "data",
+     "children": [
+      {"name": "README.md"}
+     ]
+    },
+    {"name": "images",
+     "children": [
+      {"name": "README.md"}
+     ]
+    },
+    {"name": "themes",
+     "children": [
+      {"name": "casper",
+       "children": [
+        {"name": "assets",
+         "children": [
+          {"name": "css",
+           "children": [
+           {"name": "screen.css",
+            "children": [
+              {"name": "Description of screen.css"}
+            ]}
+          ]},
+          {"name": "fonts",
+           "children": [
+            {"name": "casper-icons.eot",
+             "children": [
+              {"name": "Description of casper-icons.eot"}
+            ]},
+            {"name": "casper-icons.svg",
+             "children": [
+              {"name": "Description of casper-icons.svg"}
+            ]},
+            {"name": "casper-icons.ttf",
+             "children": [
+              {"name": "Description of casper-icons.ttf"}
+            ]},
+            {"name": "casper-icons.woff",
+             "children": [
+              {"name": "Description of casper-icons.woff"}
+            ]}
+          ]},
+          {"name": "js",
+           "children": [
+           {"name": "index.js",
+            "children": [
+              {"name": "Description of index.js"}
+            ]},
+            {"name": "jquery.fitvids.js",
+            "children": [
+              {"name": "Description of jquery.fitvids.js"}
+            ]}
+          ]}
+        ]},
+        {"name": "author.hbs",
+         "children": [
+            {"name": "Description of author.hbs"}
+        ]},
+        {"name": "default.hbs",
+         "children": [
+            {"name": "Description of default.hbs"}
+        ]},
+        {"name": "index.hbs",
+         "children": [
+            {"name": "Description of index.hbs"}
+        ]},
+        {"name": "LICENSE",
+         "children": [
+            {"name": "Description of LICENSE"}
+        ]},
+        {"name": "package.json",
+         "children": [
+            {"name": "Description of package.json"}
+        ]},
+        {"name": "page.hbs",
+         "children": [
+            {"name": "Description of page.hbs"}
+        ]},
+        {"name": "partials",
+         "children": [
+          {"name": "loop.hbs",
+           "children": [
+            {"name": "Description of loop.hbs"}
+          ]}
+        ]},
+        {"name": "post.hbs",
+         "children": [
+            {"name": "Description of post.hbs"}
+        ]},
+        {"name": "README.md",
+         "children": [
+            {"name": "Description of README.md"}
+        ]},
+        {"name": "tag.hbs",
+         "children": [
+            {"name": "Description of tag.hbs"}
+        ]}
+      ]}
+     ]
     }
    ]
   }
