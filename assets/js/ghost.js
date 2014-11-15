ff135586be028f25f327417699debf95da93d755
David Balderston <dbalders@gmail.com>
2014-11-15 00:31:48 -0800

    Core Added in, Server Next

--- assets/js/ghost.js
+++ assets/js/ghost.js
@@ -119,9 +119,132 @@
             {"name": "Description of tag.hbs"}
         ]}
       ]}
-     ]
-    }
-   ]
-  }
- ]
-}
+    ]}
+  ]},
+  {"name": "core",
+   "children": [
+    {"name": "built",
+     "children": [
+      {"name": "public",
+       "children": [
+        {"name": "jquery.js",
+         "children": [
+          {"name": "Description of jquery.js"}
+        ]},
+        {"name": "jquery.min.js",
+         "children": [
+          {"name": "Description of jquery.min.js"}
+        ]}
+      ]},
+      {"name": "scripts",
+       "children": [
+        {"name": "ghost-dev.js",
+         "children": [
+          {"name": "Description of ghost-dev.js"}
+        ]},
+        {"name": "ghost.min.js",
+         "children": [
+          {"name": "Description of ghost.min.js"}
+        ]},
+        {"name": "templates-dev.js",
+         "children": [
+          {"name": "Description of templates-dev.js"}
+        ]},
+        {"name": "vendor-dev.js",
+         "children": [
+          {"name": "Description of vendor-dev.js"}
+        ]},
+        {"name": "vendor.min.js",
+         "children": [
+          {"name": "Description of vendor.min.js"}
+        ]}
+      ]}
+    ]},
+    {"name": "client",
+       "children": [
+      {"name": "assets",
+       "children": [
+        {"name": "css",
+         "children": [
+         {"name": "ghost.min.css",
+          "children": [
+            {"name": "Description of ghost.min.css"}
+          ]}
+        ]},
+        {"name": "fonts",
+         "children": [
+         {"name": "icons.eot",
+          "children": [
+            {"name": "Description of icons.eot"}
+          ]},
+          {"name": "icons.svg",
+          "children": [
+            {"name": "Description of icons.svg"}
+          ]},
+          {"name": "icons.ttf",
+          "children": [
+            {"name": "Description of icons.ttf"}
+          ]},
+          {"name": "icons.woff",
+          "children": [
+            {"name": "Description of icons.woff"}
+          ]}
+        ]},
+        {"name": "img",
+         "children": [
+         {"name": "404-ghost@2x.png",
+          "children": [
+            {"name": "Description of 404-ghost@2x.png"}
+          ]},
+          {"name": "404-ghost.png",
+          "children": [
+            {"name": "Description of 404-ghost.png"}
+          ]},
+          {"name": "contributers",
+          "children": [
+            {"name": "Description of contributers"}
+          ]},
+          {"name": "large.png",
+          "children": [
+            {"name": "Description of large.png"}
+          ]},
+          {"name": "loadingcat.gif",
+          "children": [
+            {"name": "Description of loadingcat.gif"}
+          ]},
+          {"name": "medium.png",
+          "children": [
+            {"name": "Description of medium.png"}
+          ]},
+          {"name": "small.png",
+          "children": [
+            {"name": "Description of small.png"}
+          ]},
+          {"name": "touch-icon-ipad.png",
+          "children": [
+            {"name": "Description of touch-icon-ipad.png"}
+          ]},
+          {"name": "touch-icon-iphone.png",
+          "children": [
+            {"name": "Description of touch-icon-iphone.png"}
+          ]}
+        ]},
+        {"name": "lib",
+         "children": [
+         {"name": "touch-editor.js",
+          "children": [
+            {"name": "Description of touch-editor.js"}
+          ]},
+          {"name": "uploader.js",
+          "children": [
+            {"name": "Description of uploader.js"}
+          ]}
+        ]}
+      ]},
+      {"name": "index.js",
+       "children": [
+        {"name": "Description of index.js"}
+      ]}
+    ]}
+  ]}
+]}
