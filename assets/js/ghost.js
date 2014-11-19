a81bb675809b0947e42323f0657204e94ffdf41d
David Balderston <dbalders@gmail.com>
2014-11-18 19:16:58 -0800

    Assets, Adapters, Components

--- assets/js/ghost.js
+++ assets/js/ghost.js
@@ -123,67 +123,39 @@
   ]},
   {"name": "core",
    "children": [
-    {"name": "built",
-     "children": [
-      {"name": "public",
-       "children": [
-        {"name": "jquery.js",
-         "children": [
-          {"name": "Description of jquery.js"}
-        ]},
-        {"name": "jquery.min.js",
-         "children": [
-          {"name": "Description of jquery.min.js"}
-        ]}
-      ]},
-      {"name": "scripts",
-       "children": [
-        {"name": "ghost-dev.js",
-         "children": [
-          {"name": "Description of ghost-dev.js"}
-        ]},
-        {"name": "ghost.min.js",
-         "children": [
-          {"name": "Description of ghost.min.js"}
-        ]},
-        {"name": "templates-dev.js",
-         "children": [
-          {"name": "Description of templates-dev.js"}
-        ]},
-        {"name": "vendor-dev.js",
-         "children": [
-          {"name": "Description of vendor-dev.js"}
-        ]},
-        {"name": "vendor.min.js",
-         "children": [
-          {"name": "Description of vendor.min.js"}
-        ]}
-      ]}
-    ]},
     {"name": "client",
        "children": [
-      {"name": "assets",
-       "children": [
-        {"name": "css",
-         "children": [
-         {"name": "ghost.min.css",
+       {"name": "adapters",
+        "children": [
+          {"name": "Description of adapters"},
+          {"name": "application.js",
           "children": [
-            {"name": "Description of ghost.min.css"}
+            {"name": "Description of application.js"}
+          ]},
+          {"name": "embedded-relation-adapter.js",
+          "children": [
+            {"name": "Description of embedded-relation-adapter.js"}
+          ]},
+          {"name": "post.js",
+          "children": [
+            {"name": "Description of post.js"}
+          ]},
+          {"name": "setting.js",
+          "children": [
+            {"name": "Description of setting.js"}
+          ]},
+          {"name": "user.js",
+          "children": [
+            {"name": "Description of user.js"}
           ]}
         ]},
+      {"name": "assets",
+       "children": [
         {"name": "fonts",
          "children": [
-         {"name": "icons.eot",
-          "children": [
-            {"name": "Description of icons.eot"}
-          ]},
           {"name": "icons.svg",
           "children": [
             {"name": "Description of icons.svg"}
-          ]},
-          {"name": "icons.ttf",
-          "children": [
-            {"name": "Description of icons.ttf"}
           ]},
           {"name": "icons.woff",
           "children": [
@@ -192,17 +164,13 @@
         ]},
         {"name": "img",
          "children": [
-         {"name": "404-ghost@2x.png",
-          "children": [
-            {"name": "Description of 404-ghost@2x.png"}
-          ]},
-          {"name": "404-ghost.png",
+         {"name": "404-ghost.png",
           "children": [
             {"name": "Description of 404-ghost.png"}
           ]},
-          {"name": "contributers",
+          {"name": "404-ghost@2x.png",
           "children": [
-            {"name": "Description of contributers"}
+            {"name": "Description of 404-ghost@2x.png"}
           ]},
           {"name": "large.png",
           "children": [
@@ -239,11 +207,266 @@
           "children": [
             {"name": "Description of uploader.js"}
           ]}
+        ]},
+        {"name": "sass",
+         "children": [
+         {"name": "components",
+          "children": [
+            {"name": "Description of components"},
+            {"name":"badges.scss",
+             "children": [
+              {"name": "Description of badges.scss"}
+            ]},
+            {"name":"dropdowns.scss",
+             "children": [
+              {"name": "Description of dropdowns.scss"}
+            ]},
+            {"name":"modals.scss",
+             "children": [
+              {"name": "Description of modals.scss"}
+            ]},
+            {"name":"navigation.scss",
+             "children": [
+              {"name": "Description of navigation.scss"}
+            ]},
+            {"name":"notifications.scss",
+             "children": [
+              {"name": "Description of notifications.scss"}
+            ]},
+            {"name":"pagination.scss",
+             "children": [
+              {"name": "Description of pagination.scss"}
+            ]},
+            {"name":"popovers.scss",
+             "children": [
+              {"name": "Description of popovers.scss"}
+            ]},
+            {"name":"settings-menu.scss",
+             "children": [
+              {"name": "Description of settings-menu.scss"}
+            ]},
+            {"name":"splitbuttons.scss",
+             "children": [
+              {"name": "Description of splitbuttons.scss"}
+            ]},
+            {"name":"uploader.scss",
+             "children": [
+              {"name": "Description of uploader.scss"}
+            ]}
+          ]},
+          {"name": "layouts",
+          "children": [
+            {"name": "Layouts is where the css goes for each individual page. You can see each page's css is broken down into its own file. These are changes made on top of the global settings from the patterns folder"},
+            {"name":"about.scss",
+             "children": [
+              {"name": "Description of about.scss"}
+            ]},
+            {"name":"auth.scss",
+             "children": [
+              {"name": "Description of auth.scss"}
+            ]},
+            {"name":"content.scss",
+             "children": [
+              {"name": "Description of content.scss"}
+            ]},
+            {"name":"default.scss",
+             "children": [
+              {"name": "Description of default.scss"}
+            ]},
+            {"name":"editor.scss",
+             "children": [
+              {"name": "Description of editor.scss"}
+            ]},
+            {"name":"error.scss",
+             "children": [
+              {"name": "Description of error.scss"}
+            ]},
+            {"name":"settings.scss",
+             "children": [
+              {"name": "Description of settings.scss"}
+            ]},
+            {"name":"setup.scss",
+             "children": [
+              {"name": "Description of setup.scss"}
+            ]},
+            {"name":"tags.scss",
+             "children": [
+              {"name": "Description of tags.scss"}
+            ]},
+            {"name":"user.scss",
+             "children": [
+              {"name": "Description of user.scss"}
+            ]},
+            {"name":"users.scss",
+             "children": [
+              {"name": "Description of users.scss"}
+            ]}
+          ]},
+          {"name": "modules",
+          "children": [
+            {"name": "Description of modules"},
+            {"name":"animations.scss",
+             "children": [
+              {"name": "Description of animations.scss"}
+            ]},
+            {"name":"icons.scss",
+             "children": [
+              {"name": "Description of icons.scss"}
+            ]},
+            {"name":"mixins.scss",
+             "children": [
+              {"name": "Description of mixins.scss"}
+            ]},
+            {"name":"variables.scss",
+             "children": [
+              {"name": "Description of variables.scss"}
+            ]}
+          ]},
+          {"name": "patterns",
+          "children": [
+            {"name": "Patterns is where all the global attributes are held. They are broken down into categories then styled for the entire blog"},
+            {"name":"_shame.scss",
+             "children": [
+              {"name": "Description of _shame.scss"}
+            ]},
+            {"name":"buttons.scss",
+             "children": [
+              {"name": "Description of buttons.scss"}
+            ]},
+            {"name":"forms.scss",
+             "children": [
+              {"name": "Description of forms.scss"}
+            ]},
+            {"name":"global.scss",
+             "children": [
+              {"name": "Description of global.scss"}
+            ]},
+            {"name":"labels.scss",
+             "children": [
+              {"name": "Description of labels.scss"}
+            ]},
+            {"name":"navlist.scss",
+             "children": [
+              {"name": "Description of navlist.scss"}
+            ]},
+            {"name":"tables.scss",
+             "children": [
+              {"name": "Description of tables.scss"}
+            ]}
+          ]},
+          {"name": "vendor",
+          "children": [
+            {"name": "Description of vendor"},
+            {"name":"codemirror.scss",
+             "children": [
+              {"name": "Description of codemirror.scss"}
+            ]},
+            {"name":"nanoscroller.scss",
+             "children": [
+              {"name": "Description of nanoscroller.scss"}
+            ]},
+            {"name":"nprogress.scss",
+             "children": [
+              {"name": "Description of nprogress.scss"}
+            ]}
+          ]},
+          {"name":"screen.scss",
+           "children": [
+            {"name": "Description of screen.scss"}
+          ]}
         ]}
       ]},
-      {"name": "index.js",
+      {"name": "components",
        "children": [
-        {"name": "Description of index.js"}
+        {"name": "Description of components"},
+        {"name": "gh-activating-list-item.js.js",
+        "children": [
+          {"name": "Description of gh-activating-list-item.js.js"}
+        ]},
+        {"name": "gh-codemirror.js",
+        "children": [
+          {"name": "Description of gh-codemirror.js"}
+        ]},
+        {"name": "gh-dropdown-button.js",
+        "children": [
+          {"name": "Description of gh-dropdown-button.js"}
+        ]},
+        {"name": "gh-dropdown.js",
+        "children": [
+          {"name": "Description of gh-dropdown.js"}
+        ]},
+        {"name": "gh-file-upload.js",
+        "children": [
+          {"name": "Description of gh-file-upload.js"}
+        ]},
+        {"name": "gh-form.js",
+        "children": [
+          {"name": "Description of gh-form.js"}
+        ]},
+        {"name": "gh-input.js",
+        "children": [
+          {"name": "Description of gh-input.js"}
+        ]},
+        {"name": "gh-markdown.js",
+        "children": [
+          {"name": "Description of gh-markdown.js"}
+        ]},
+        {"name": "gh-modal-dialog.js",
+        "children": [
+          {"name": "Description of gh-modal-dialog.js"}
+        ]},
+        {"name": "gh-notification.js",
+        "children": [
+          {"name": "Description of gh-notification.js"}
+        ]},
+        {"name": "gh-notifications.js",
+        "children": [
+          {"name": "Description of gh-notifications.js"}
+        ]},
+        {"name": "gh-popover-button.js",
+        "children": [
+          {"name": "Description of gh-popover-button.js"}
+        ]},
+        {"name": "gh-popover.js",
+        "children": [
+          {"name": "Description of gh-popover.js"}
+        ]},
+        {"name": "gh-role-selector.js",
+        "children": [
+          {"name": "Description of gh-role-selector.js"}
+        ]},
+        {"name": "gh-select.js",
+        "children": [
+          {"name": "Description of gh-select.js"}
+        ]},
+        {"name": "gh-tab-pane.js",
+        "children": [
+          {"name": "Description of gh-tab-pane.js"}
+        ]},
+        {"name": "gh-tab.js",
+        "children": [
+          {"name": "Description of gh-tab.js"}
+        ]},
+        {"name": "gh-tabs-manager.js",
+        "children": [
+          {"name": "Description of gh-tabs-manager.js"}
+        ]},
+        {"name": "gh-textarea.js",
+        "children": [
+          {"name": "Description of gh-textarea.js"}
+        ]},
+        {"name": "gh-trim-focus-input.js",
+        "children": [
+          {"name": "Description of gh-trim-focus-input.js"}
+        ]},
+        {"name": "gh-upload-modal.js",
+        "children": [
+          {"name": "Description of gh-upload-modal.js"}
+        ]},
+        {"name": "gh-uploader.js",
+        "children": [
+          {"name": "Description of gh-uploader.js"}
+        ]},
       ]}
     ]}
   ]}
